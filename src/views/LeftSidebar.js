aa328b152be5fba72cf5d9a1a1a06c744020dc42
deniztahmouresi <d.tahmouresi@gmail.com>
2023-10-11 16:21:42 +0330

    a simple portfolio

---
+++ src/views/LeftSidebar.js
@@ -0,0 +1,12 @@
+import '../assets/css/components/LeftSidebar.css';
+
+function LeftSidebar(){
+
+    return(
+        <div className="left-sidebar hide-min">
+            <div className="profile-pic"></div>
+        </div>
+    );
+}
+
+export default LeftSidebar;
